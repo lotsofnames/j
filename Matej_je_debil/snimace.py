c38572a60b4319addf61f98d22e5732917a99969
matus <matussadovsky99@gmail.com>
2025-09-30 14:17:56 +0200

    .

--- Matej_je_debil/snimace.py
+++ Matej_je_debil/snimace.py
@@ -1,9 +1,15 @@
 #Indukčné snímače
-from word import file
 import streamlit
 st=streamlit
 #st.link_button("Indukčné snímače","https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1")
-
+def file(z):
+    slova=[]
+    with open(z,"r",encoding="utf-8") as fill:
+        fill = fill.readlines()
+        for line in fill:
+            if line.replace(" ", "") != ""and line.replace(" ", "") != "\n":
+                slova.append(line.replace("\n",""))
+    return slova
 slova=file("IS.txt")
 for slovo in slova:
     if slovo.startswith("#"):
