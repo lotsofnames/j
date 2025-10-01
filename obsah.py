69f1faed42cd31a65b9af374604f136fc88447e4
matus <matussadovsky99@gmail.com>
2025-10-01 17:01:30 +0200

    .

--- obsah.py
+++ obsah.py
@@ -3,7 +3,12 @@ import streamlit
 st = streamlit
 st.title("Obsah")
 st.link_button(
-    "Indukčné snímače", "https://hhb69am89khxtkmdpxcdnv.streamlit.app/#indukcne-snimace"
+    "Indukčné snímače",
+    "https://hhb69am89khxtkmdpxcdnv.streamlit.app/#indukcne-snimace"
+)
+st.link_button(
+    "Kapacitné snímače",
+    "https://lotsofnames-j-ks-sem2e3.streamlit.app/#kapacitne-snimace",
 )
 st.link_button(
     "Rickrolling",
