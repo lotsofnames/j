95793f218d7165886d97910bd8b6837478aeb17b
matus <matussadovsky99@gmail.com>
2025-10-01 16:58:55 +0200

    .

--- funkcion/obsah.py
+++ funkcion/obsah.py
@@ -6,6 +6,10 @@ st.link_button(
     "Indukčné snímače", "https://hhb69am89khxtkmdpxcdnv.streamlit.app/#indukcne-snimace"
 )
 st.link_button(
+    "Kapacitné snímače",
+    "https://lotsofnames-j-ks-sem2e3.streamlit.app/#kapacitne-snimace",
+)
+st.link_button(
     "Rickrolling",
     "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1",
 )
