3e7506e3656a215aaa411c296d3c68b5a4d1e41f
matus <matussadovsky99@gmail.com>
2025-10-05 09:37:19 +0200

    .

---
+++ NTC.py
@@ -0,0 +1,14 @@
+import os
+import funkcion.slovo
+
+def NTC():
+    base = os.path.dirname(__file__)
+    base = os.path.join(base, "NTC")
+    # print(base)
+
+    paths = os.path.join(base, "NTC.txt")
+    # print(paths)
+
+
+    funkcion.slovo.main(base, paths)
+NTC()
