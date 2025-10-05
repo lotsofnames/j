f6cab5b618fd4700d27286db6cffc4ac4a5142bb
matus <matussadovsky99@gmail.com>
2025-10-05 09:21:49 +0200

    .

---
+++ SUV.py
@@ -0,0 +1,14 @@
+import os
+import funkcion.slovo
+
+def SUV():
+    base = os.path.dirname(__file__)
+    base = os.path.join(base, "SUV")
+    # print(base)
+
+    paths = os.path.join(base, "SUV.txt")
+    # print(paths)
+
+
+    funkcion.slovo.main(base, paths)
+SUV()
