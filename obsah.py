034481d41c753a52f1d14e057c59767e6ec791e6
matus <matussadovsky99@gmail.com>
2025-10-10 19:58:41 +0200

    .

--- obsah.py
+++ obsah.py
@@ -1,4 +1,5 @@
 import streamlit
+import os
 from MS import MS
 from KS import KS
 from IS import IS
@@ -12,43 +13,29 @@ from SUV import SUV
 from NTC import NTC
 
 st = streamlit
+base = os.path.dirname(__file__)
+base = os.path.join(base, "QR","adres.txt")
 st.title("Obsah")
-st.link_button(
+img_of = [
     "Indukčné snímače",
-    "https://lotsofnames-j-is-k3unmc.streamlit.app/#indukcne-snimace",
-)
-st.link_button(
     "Kapacitné snímače",
-    "https://lotsofnames-j-ks-sem2e3.streamlit.app/#kapacitne-snimace",
-)
-st.link_button(
     "Magnetické snímače",
-    "https://lotsofnames-j-ms-yuipbp.streamlit.app/#magneticke-snimace",
-)
-st.link_button(
-    "Optické snímače", "https://lotsofnames-j-os-kihfbl.streamlit.app/#opticke-snimace"
-)
-st.link_button(
-    "PIR Snímače", "https://lotsofnames-j-pir-xcsgla.streamlit.app/#pir-snimace"
-)
-st.link_button(
+    "Optické snímače",
+    "PIR Snímače",
     "Senzor úniku vody",
-    "https://zq73gtflpyeysronrzwzhs.streamlit.app/#senzor-uniku-vody",
-)
-st.link_button(
-    "Snímač nárazu", "https://lotsofnames-j-sn-lyfriu.streamlit.app/#snimac-narazu"
-)
-st.link_button(
+    "Snímač nárazu",
     "Snímač teploty NTC",
-    "https://lotsofnames-j-ntc-mljdaj.streamlit.app/#snimac-teploty-ntc",
-)
-st.link_button(
-    "Snímače plameňa", "https://lotsofnames-j-sp-efgppi.streamlit.app/#snimace-plamena"
-)
-st.link_button(
+    "Snímače plameňa",
     "Ultrazvukové snímače",
-    "https://lotsofnames-j-us-ogirnx.streamlit.app/#ultrazvukove-snimace",
-)
-st.link_button(
-    "Zvukové snímače", "https://lotsofnames-j-zs-tsjazt.streamlit.app/#zvukove-snimace"
-)
+    "Zvukové snímače",
+]
+i=0
+with open(base, "r") as f:
+    for line in f:
+        line = line.strip().replace("\n", "")
+        st.link_button(
+            img_of[i],
+            line,
+        )
+        i=i+1
+
