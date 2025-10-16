b7a4af4412b3bf76f00a122310a4856368c6f4d5
lotsofnames <116421444+lotsofnames@users.noreply.github.com>
2025-10-16 11:52:06 +0200

    .

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from streamlit import title
+
+pg=st.navigation([st.Page("IS.py",title="Indukčné snímače"),st.Page("KS.py",title="Kapacitné snímače"),st.Page("MS.py",title="Magnetické snímače"),st.Page("NTC.py",title="Snímač teploty NTC"),st.Page("OS.py",title="Optické snímače"),st.Page("PIR.py",title="PIR Snímače"),st.Page("SN.py",title="Snímač nárazu"),st.Page("SP.py",title="Snímače plameňa"),st.Page("SUV.py",title="Senzor úniku vody"),st.Page("US.py",title="Ultrazvukové snímače"),st.Page("ZS.py",title="Zvukové snímače"),])
+pg.run()
