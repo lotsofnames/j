94a717dbbedb77f7d04753d276b34f7c5896e84a
matus <matussadovsky99@gmail.com>
2025-10-20 17:01:11 +0200

    .

---
+++ qrcodeweb.py
@@ -0,0 +1,9 @@
+import qrcode
+import streamlit as st
+st.title("QR Code")
+content = st.text_input("Enter the content of qr code:")
+if st.button("Generate QR Code"):
+    qrcode.make(content).save("qrcode.png")
+    st.image("qrcode.png")
+    with open("qrcode.png", "rb") as f:
+        st.download_button(label="Download QR Code", data=f, file_name="qrcode.png",mime="image/png")
