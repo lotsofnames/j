f6c75b172e8b23f865bb5f7739188ff312c23aec
lotsofnames <116421444+lotsofnames@users.noreply.github.com>
2025-11-24 07:50:11 +0100

    .

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import os
-from streamlit import title
-port = int(os.environ.get("PORT", 8501))  # 8501 je default u Streamlit, ale Azure môže dať inú hodnotu
 pg = st.navigation(
     {
         "info": [
